0733683ad2fa43196f5b663829d773fb02ee5344
eQuiNoX__ <equinox.71717171@gmail.com>
2015-04-25 14:32:54 +0200

    Improved logging

--- test_oracle_basic.py
+++ test_oracle_basic.py
@@ -1,6 +1,7 @@
 import urllib
 from oracle import PaddingAttack
 import logging
+import Colorer
 
 sample_encrypted = "401103ab592440fbc1f6b5e9c65a8d0c36686e296e0541830fae55351d160c38a36589c41b6e0d40a71853a930be2364c85e2e1999ac3878270895228c60e8ac"
 
@@ -15,14 +16,17 @@ def try_sending(enc_block):
 
 
 def main():
-    p = PaddingAttack(sample_encrypted[0:32], "LOLOLOLOLOLOLOLO".encode("hex"), try_sending, 16, logging.DEBUG)
-    print p.attack()
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("test_oracle_basic")
+
+    p = PaddingAttack(sample_encrypted[0:32], "LOLOLOLOLOLOLOLO".encode("hex"), try_sending, 16)
+    logger.info(p.attack())
     p = PaddingAttack(sample_encrypted[32:64], sample_encrypted[0:32], try_sending, 16)
-    print p.attack()
+    logger.info(p.attack())
     p = PaddingAttack(sample_encrypted[64:96], sample_encrypted[32:64], try_sending, 16)
-    print p.attack()
+    logger.info(p.attack())
     p = PaddingAttack(sample_encrypted[96:128], sample_encrypted[64:96], try_sending, 16)
-    print p.attack()
+    logger.info(p.attack())
 
 if __name__ == '__main__':
     main()
