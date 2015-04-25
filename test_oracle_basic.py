c1543664fede907fb6bba8cf734a5dffea570973
eQuiNoX__ <equinox.71717171@gmail.com>
2015-04-25 14:37:21 +0200

    Make the test more usable

--- test_oracle_basic.py
+++ test_oracle_basic.py
@@ -1,6 +1,9 @@
 import urllib
-from oracle import PaddingAttack
 import logging
+import sys
+
+from oracle import PaddingAttack
+
 import Colorer
 
 sample_encrypted = "401103ab592440fbc1f6b5e9c65a8d0c36686e296e0541830fae55351d160c38a36589c41b6e0d40a71853a930be2364c85e2e1999ac3878270895228c60e8ac"
@@ -16,7 +19,11 @@ def try_sending(enc_block):
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    if sys.argv[-1] == "-d":
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        print "For DEBUG info pass in the '-d' flag at the command line."
+        logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger("test_oracle_basic")
 
     p = PaddingAttack(sample_encrypted[0:32], "LOLOLOLOLOLOLOLO".encode("hex"), try_sending, 16)
